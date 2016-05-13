44a16d8a424213b31f139f719d6f5bce347d1d18
Steven Costa <scosta@nextgen.com>
2016-05-13 08:38:29 -0400

    Initial fork

---
+++ CodeFirstStoreFunctionsTests/DbFunctionDetailsAttributeTests.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Pawel Kadluczka, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+
+namespace CodeFirstStoreFunctions
+{
+    using Xunit;
+
+    public class DbFunctionDetailsAttributeTests
+    {
+        [Fact]
+        public void Can_set_get_schema()
+        {
+            var attr = new DbFunctionDetailsAttribute();
+
+            Assert.Null(attr.DatabaseSchema);
+
+            attr.DatabaseSchema = "dbo";
+
+            Assert.Equal("dbo", attr.DatabaseSchema);
+        }
+
+        [Fact]
+        public void Can_set_get_ResultColumnName()
+        {
+            var attr = new DbFunctionDetailsAttribute();
+
+            Assert.Null(attr.ResultColumnName);
+
+            attr.ResultColumnName = "column";
+
+            Assert.Equal("column", attr.ResultColumnName);
+        }
+
+        [Fact]
+        public void Can_get_set_ResultTypes()
+        {
+            var resultTypes = new Type[0];
+            var attr = new DbFunctionDetailsAttribute();
+
+            Assert.Null(attr.ResultTypes);
+
+            attr.ResultTypes = resultTypes;
+
+            Assert.Same(resultTypes, attr.ResultTypes);
+        }
+    }
+}
